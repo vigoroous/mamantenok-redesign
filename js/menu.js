d521bbf0fa4e7ee950b2ed6287596f32e90204db
Bogdan Martyshevskiy <vigoroous@gmail.com>
2021-07-06 10:49:01 +0300

    Submenu & content__cover created

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,37 @@
+let menu = document.querySelector(".menu");
 let burger = document.querySelector(".menu__burger");
-let opened = false;
 
-console.log(burger);
+let submenu_link = document.querySelector(".menu__nav-item.submenu > .menu__nav-link");
+let submenu_list = submenu_link.closest(".menu__nav-item.submenu").querySelector(".menu__submenu-list")
+
+let menu_opened = false;
+let submenu_opened = false;
 
 burger.onclick = function (event) {
-    
-    if (!opened) {
+    if (!menu_opened) {
         burger.classList.add("menu__burger_opened");
-        opened = true;
+        menu.classList.remove("menu_closed");
+        menu.classList.add("menu_opened");
+        menu_opened = true;
     }
     else {
         burger.classList.remove("menu__burger_opened");
-        opened = false;
+        menu.classList.add("menu_closed");
+        menu.classList.remove("menu_opened");
+        menu_opened = false;
     }
-    //alert("Бургер.");
-
 }
+
+submenu_link.addEventListener("mouseover", function (event) {
+    if (menu_opened) {
+        menu.setAttribute("style", `height:${menu.clientHeight + submenu_list.clientHeight}px`);
+        menu.style.height = menu.clientHeight + submenu_list.clientHeight;
+    }
+}, false);
+
+submenu_link.addEventListener("mouseout", function (event) {
+    if (menu_opened) {
+        menu.removeAttribute("style");
+        menu.style.height = menu.clientHeight - submenu_list.clientHeight;
+    }
+}, false);
