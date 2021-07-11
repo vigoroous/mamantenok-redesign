66b9deb106ff44cc1cd2b53c41740ba8533188ba
Bogdan Martyshevskiy <vigoroous@gmail.com>
2021-07-11 03:06:29 +0300

    Menu reworked

--- js/menu.js
+++ js/menu.js
@@ -11,27 +11,13 @@ burger.onclick = function (event) {
     if (!menu_opened) {
         burger.classList.add("menu__burger_opened");
         menu.classList.remove("menu_closed");
-        menu.classList.add("menu_opened");
+        // menu.classList.add("menu_opened");
         menu_opened = true;
     }
     else {
         burger.classList.remove("menu__burger_opened");
         menu.classList.add("menu_closed");
-        menu.classList.remove("menu_opened");
+        // menu.classList.remove("menu_opened");
         menu_opened = false;
     }
 }
-
-submenu_link.addEventListener("mouseover", function (event) {
-    if (menu_opened) {
-        menu.setAttribute("style", `height:${menu.clientHeight + submenu_list.clientHeight}px`);
-        menu.style.height = menu.clientHeight + submenu_list.clientHeight;
-    }
-}, false);
-
-submenu_link.addEventListener("mouseout", function (event) {
-    if (menu_opened) {
-        menu.removeAttribute("style");
-        menu.style.height = menu.clientHeight - submenu_list.clientHeight;
-    }
-}, false);
